cea4a4e1fdd966a020fc96a8de4c9d2c4cd7d53c
Crislei <100581016+Jamilly-arouche@users.noreply.github.com>
2022-08-24 15:07:28 -0300

    .

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 let numero = 0
 
-
 function aumentar(){
     numero = numero + 1
     mostrarnatela()
 }
 
-
 function diminuir(){
     numero = numero -1
     mostrarnatela()
 }
-
 
 function mostrarnatela(){
     const p = document.querySelector("p")
