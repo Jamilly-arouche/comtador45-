7476d102b92e537fce02ad453e2caf98fcd254f0
Jamilly Costa Arouche <100581016+Jamilly-arouche@users.noreply.github.com>
2022-08-24 15:01:35 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+let numero = 0
+
+
+function aumentar(){
+    numero = numero + 1
+    mostrarnatela()
+}
+
+
+function diminuir(){
+    numero = numero -1
+    mostrarnatela()
+}
+
+
+function mostrarnatela(){
+    const p = document.querySelector("p")
+    p.innerText = numero
+}
